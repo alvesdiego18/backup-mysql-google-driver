de626c3be3faa6b33766b39a92372baba89bb1fa
Diego Alves <alvesdiego18@gmail.com>
2021-02-25 01:40:58 -0300

    bug fix: object removed from backup instance

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 const { monitor_backup } = require(`./dist`)
-// module.exports = monitor_backup
-
-monitor_backup({
-
-    backup: {
-        host: 'localhost',
-        user: 'root',
-        password: 'mysql',
-        database: 'unlock',
-        keep_files: 2,
-    }
-})
+module.exports = monitor_backup
